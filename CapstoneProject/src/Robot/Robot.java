400915c3ce06793f934d0fe15574b0a5617229d6
Deeptesh Dey <89153855+ddey008@users.noreply.github.com>
2022-05-06 10:04:33 -0700

    Update Robot.java

--- CapstoneProject/src/Robot/Robot.java
+++ CapstoneProject/src/Robot/Robot.java
@@ -1,5 +1,34 @@
 package Robot;
 
 public class Robot {
+private int Health;
+private double speed;
+private int Damage;
+private double reload;
+private Weapon weapon;
+private Armor armor;
+private Ability ability;
+
+public Robot(Weapon weapon , Armor armor , Ability ability) {
+ this.weapon= weapon;
+ this.armor = armor;
+ this.ability = ability;
+ Health = 150;
+ speed = 10;
+ Damage = 20;
+ reload = 5;
+}
+public void setDamage() {
+	
+}
+public void setReload() {
+	
+}
+public void setSpeed() {
+	
+}
+public void setHealth() {
+	
+}
 
 }
